ac785c40d01a4cd3073c91c2a9a9fcd97a9c6b80
Eduard <info@did-web.com>
2025-08-08 16:43:15 +0200

    Create Circle trae le circle creado

--- src/src/Controller/Circle/CircleCreateController.php
+++ src/src/Controller/Circle/CircleCreateController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Controller\Circle;
 
+use App\DTO\Circle\CircleGetDto;
+use App\Utils\JsonResponseFactory;
 use App\DTO\Circle\CircleCreateDto;
+use App\Service\Circle\CircleGetService;
+use App\Utils\AuthenticatedUserInterface;
 use App\Request\Circle\CircleCreateRequest;
 use App\Service\Circle\CircleCreateService;
-use App\Utils\JsonResponseFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +20,8 @@ class CircleCreateController extends AbstractController
     public function __construct(
         private CircleCreateService $createCircleService,
         private CircleCreateRequest $circleCreateRequest,
+        private CircleGetService $circleGetService,
+        private AuthenticatedUserInterface $authUser
     ) {}
 
     public function __invoke(Request $request): JsonResponse
@@ -27,8 +32,14 @@ class CircleCreateController extends AbstractController
 
         $cirleCreateDto = new CircleCreateDto($validData['name'],$validData['color']);
 
-        $this->createCircleService->create($this->getUser(), $cirleCreateDto);
+        $idCircle = $this->createCircleService->create($this->getUser(), $cirleCreateDto);
 
-        return JsonResponseFactory::success(['message' => 'Círculo creado correctamente'], Response::HTTP_CREATED);
+        $circleGetDto = new CircleGetDto($idCircle);
+        $circle = $this->circleGetService->get($circleGetDto, $this->authUser);
+
+
+        return JsonResponseFactory::success([
+            'message' => 'Círculo creado correctamente', 
+            'circle'=> $circle], Response::HTTP_CREATED);
     }
 }
